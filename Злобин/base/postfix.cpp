b37da9ff68a50523b203096f0e0e965fd6a2ccd1
ZlobinGM <geozlo8738@gmail.com>
2019-10-13 18:07:41 +0300

    Ver 0.1 add stack

---
+++ Злобин/base/postfix.cpp
@@ -0,0 +1,20 @@
+﻿#include "postfix.h"
+#include "stack.h"
+
+void TPostfix::SetInfix(const string& str)
+{
+	if (str == infix)return;
+	infix = str;
+	postfix = "";
+}
+
+string TPostfix::ToPostfix()
+{
+  postfix = string("ab+");
+  return postfix;
+}
+
+double TPostfix::Calculate()
+{
+  return 0;
+}
---
+++ Злобин/base/postfix.h
@@ -0,0 +1,22 @@
+﻿#ifndef __POSTFIX_H__
+#define __POSTFIX_H__
+
+#include <string>
+#include "stack.h"
+
+using namespace std;
+
+class TPostfix
+{
+	string infix;
+	string postfix;
+public:
+	TPostfix(const string& str = "") : infix(str) { postfix = ""; }
+	void SetInfix(const string& str);
+	string GetInfix() { return infix; }
+	string GetPostfix() { return postfix; }
+	string ToPostfix();
+	double Calculate(); // Ввод переменных, вычисление по постфиксной форме
+};
+
+#endif
